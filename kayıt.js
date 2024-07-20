b42b65c2c7308e52cd3aeb0177c602342d9e829d
Muhammet Emin <66025604+MuhammetDev@users.noreply.github.com>
2024-07-20 20:26:40 +0300

    Update kayıt.js

--- kayıt.js
+++ kayıt.js
@@ -6,136 +6,135 @@ module.exports = {
   aliases: ["k"],
   async execute(client, message, args) {
 
-  // Gerekli rol ID'leri ve log kanalı ID'si
-  const kayitsizRoleID = '1264249388516249631'; // Kayıtsız rolünün ID'sini burada değiştirin
-  const kizRoleID = '1263218335361929256'; // Kız rolünün ID'sini burada değiştirin
-  const erkekRoleID = '11264249044071612500'; // Erkek rolünün ID'sini burada değiştirin
-  const logChannelID = '1242942289211560039'; // Log kanalının ID'sini burada değiştirin
-  const yetkiliRoleID = '1264252101937070242'; // Yetkili rolünün ID'sini burada değiştirin
+    // Gerekli rol ID'leri ve log kanalı ID'si
+    const kayitsizRoleID = '1264249388516249631'; // Kayıtsız rolünün ID'sini burada değiştirin
+    const kizRoleID = '1263218335361929256'; // Kız rolünün ID'sini burada değiştirin
+    const erkekRoleID = '11264249044071612500'; // Erkek rolünün ID'sini burada değiştirin
+    const logChannelID = '1242942289211560039'; // Log kanalının ID'sini burada değiştirin
+    const yetkiliRoleID = '1264252101937070242'; // Yetkili rolünün ID'sini burada değiştirin
 
-  // Komutu sadece belirli bir rolü olan yetkililer kullanabilir
-  if (!message.member.roles.cache.has(yetkiliRoleID)) {
-    return message.channel.send("Bu komutu kullanmak için yeterli izniniz yok.");
-  }
+    // Komutu sadece belirli bir rolü olan yetkililer kullanabilir
+    if (!message.member.roles.cache.has(yetkiliRoleID)) {
+      return message.channel.send("Bu komutu kullanmak için yeterli izniniz yok.");
+    }
 
-  // Komut argümanları kontrolü
-  if (args.length < 2) {
-    return message.channel.send("Lütfen şu formatta komutu kullanın: .kayıt <id/@üye> <isim> <yaş>");
-  }
+    // Komut argümanları kontrolü
+    if (args.length < 2) {
+      return message.channel.send("Lütfen şu formatta komutu kullanın: .kayıt <id/@üye> <isim>");
+    }
 
-  const userMention = args[0];
-  const name = args[1];
-  const age = args[2];
+    const userMention = args[0];
+    const name = args[1];
 
-  // Kullanıcıyı bulma
-  let user;
-  if (message.mentions.members.size) {
-    user = message.mentions.members.first();
-  } else {
-    user = message.guild.members.cache.get(userMention);
-  }
+    // Kullanıcıyı bulma
+    let user;
+    if (message.mentions.members.size) {
+      user = message.mentions.members.first();
+    } else {
+      user = message.guild.members.cache.get(userMention);
+    }
 
-  if (!user) {
-    return message.channel.send("Belirtilen kullanıcı bulunamadı.");
-  }
+    if (!user) {
+      return message.channel.send("Belirtilen kullanıcı bulunamadı.");
+    }
 
-  const row = new ActionRowBuilder()
-    .addComponents(
-      new ButtonBuilder()
-        .setCustomId('register_kiz')
-        .setLabel('Kız')
-        .setEmoji('<:Kadin:1262016418359017515>')
-        .setStyle(ButtonStyle.Primary),
-      new ButtonBuilder()
-        .setCustomId('register_erkek')
-        .setLabel('Erkek')
-        .setEmoji('<:Erkek:1262016370896277614>')
-        .setStyle(ButtonStyle.Primary)
-    );
+    const row = new ActionRowBuilder()
+      .addComponents(
+        new ButtonBuilder()
+          .setCustomId('register_kiz')
+          .setLabel('Kız')
+          .setEmoji('<:Kadin:1262016418359017515>')
+          .setStyle(ButtonStyle.Primary),
+        new ButtonBuilder()
+          .setCustomId('register_erkek')
+          .setLabel('Erkek')
+          .setEmoji('<:Erkek:1262016370896277614>')
+          .setStyle(ButtonStyle.Primary)
+      );
 
-  const embed = new EmbedBuilder()
-    .setTitle(`Yeni bir üye kayıt ediliyor<a:exdoki_aaaaa:1262017677916901518>`)
-    .setDescription(`**Lütfen cinsiyetinizi seçiniz`)
-    .addFields({name: `**Kayıt edilen kullanıcı**`, value: `<@${user.id}>`},
-      {name: `**İsmi**`, value: `\`${name}\``},
-      {name: `**Yaşı**`, value: `\`${age}\``})
-    .setThumbnail(client.user.displayAvatarURL({ dynamic: true, format: "png", size: 1024 }))
-    .setTimestamp()
-  message.channel.send({ embeds: [embed], components: [row] })
-    .then(sentMessage => {
-      const collector = sentMessage.createMessageComponentCollector({ filter: i => i.user.id === message.author.id, time: 60000 });
+    const embed = new EmbedBuilder()
+      .setTitle(`Yeni bir üye kayıt ediliyor<a:exdoki_aaaaa:1262017677916901518>`)
+      .setDescription(`**Lütfen cinsiyetinizi seçiniz`)
+      .addFields({name: `**Kayıt edilen kullanıcı**`, value: `<@${user.id}>`},
+        {name: `**İsmi**`, value: `\`${name}\``})
+      .setThumbnail(client.user.displayAvatarURL({ dynamic: true, format: "png", size: 1024 }))
+      .setTimestamp()
+    message.channel.send({ embeds: [embed], components: [row] })
+      .then(sentMessage => {
+        const collector = sentMessage.createMessageComponentCollector({ filter: i => i.user.id === message.author.id, time: 60000 });
 
-      collector.on('collect', async interaction => {
-        if (!user) {
-          return interaction.reply({ content: 'Kullanıcı bulunamadı.', ephemeral: true });
-        }
-
-        let roleID;
-        if (interaction.customId === 'register_kiz') {
-          roleID = kizRoleID;
-        } else if (interaction.customId === 'register_erkek') {
-          roleID = erkekRoleID;
-        }
-
-        if (roleID) {
-          await user.roles.add(roleID);
-          await user.roles.remove(kayitsizRoleID);
-          await user.setNickname(`${name} | ${age}`);
-
-          const logEmbed = new EmbedBuilder()
-            .setTitle('Kayıt Log')
-            .addFields(
-              { name: 'Kayıt Edilen Kullanıcı', value: `<@${user.id}>` },
-              { name: 'Yetkili', value: `<@${message.author.id}>` },
-              { name: 'Cinsiyet', value: interaction.customId === 'register_kiz' ? 'Kız' : 'Erkek' },
-              { name: 'İsim | Yaş', value: `${name} | ${age}` }
-            )
-            .setTimestamp();
-
-          const logChannel = client.channels.cache.get(logChannelID);
-          if (logChannel) {
-            logChannel.send({ embeds: [logEmbed] });
+        collector.on('collect', async interaction => {
+          if (!user) {
+            return interaction.reply({ content: 'Kullanıcı bulunamadı.', ephemeral: true });
           }
 
-          // İsim değişikliklerini kaydet
-          let usernameChanges = {};
-          if (fs.existsSync('usernameChanges.json')) {
-            usernameChanges = JSON.parse(fs.readFileSync('usernameChanges.json', 'utf8'));
+          let roleID;
+          if (interaction.customId === 'register_kiz') {
+            roleID = kizRoleID;
+          } else if (interaction.customId === 'register_erkek') {
+            roleID = erkekRoleID;
           }
-          if (!usernameChanges[user.id]) {
-            usernameChanges[user.id] = [];
+
+          if (roleID) {
+            await user.roles.add(roleID);
+            await user.roles.remove(kayitsizRoleID);
+            await user.setNickname(`${name}`);
+
+            const logEmbed = new EmbedBuilder()
+              .setTitle('Kayıt Log')
+              .addFields(
+                { name: 'Kayıt Edilen Kullanıcı', value: `<@${user.id}>` },
+                { name: 'Yetkili', value: `<@${message.author.id}>` },
+                { name: 'Cinsiyet', value: interaction.customId === 'register_kiz' ? 'Kız' : 'Erkek' },
+                { name: 'İsim', value: `${name}` }
+              )
+              .setTimestamp();
+
+            const logChannel = client.channels.cache.get(logChannelID);
+            if (logChannel) {
+              logChannel.send({ embeds: [logEmbed] });
+            }
+
+            // İsim değişikliklerini kaydet
+            let usernameChanges = {};
+            if (fs.existsSync('usernameChanges.json')) {
+              usernameChanges = JSON.parse(fs.readFileSync('usernameChanges.json', 'utf8'));
+            }
+            if (!usernameChanges[user.id]) {
+              usernameChanges[user.id] = [];
+            }
+            usernameChanges[user.id].push({
+              oldName: user.nickname || user.user.username,
+              newName: `${name}`,
+              changedBy: `<@${message.author.id}>`,
+              method: '**Kayıt**',
+              date: new Date().toISOString()
+            });
+            fs.writeFileSync('usernameChanges.json', JSON.stringify(usernameChanges, null, 2));
+
+            // Embed'i güncelle
+            const updatedEmbed = new EmbedBuilder()
+              .setTitle('Üye başarıyla kayıt edildi.')
+              .addFields({name: `**Yetkili**`, value: `<@${message.author.id}>`},
+              {name: `**Kayıt Edilen Kullanıcı**`, value: `<@${user.id}>`},
+              {name: `**İsmi**`, value: `${name}`},
+              {name: `Cinsiyeti`, value: `${interaction.customId === 'register_kiz' ? 'Kız' : 'Erkek'}`}
+              )
+              .setThumbnail(client.user.displayAvatarURL({ dynamic: true, format: "png", size: 1024 }))
+              .setTimestamp()
+            await interaction.update({ embeds: [updatedEmbed], components: [] });
           }
-          usernameChanges[user.id].push({
-            oldName: user.nickname || user.user.username,
-            newName: `${name} | ${age}`,
-            changedBy: `<@${message.author.id}>`,
-            method: '**Kayıt**',
-            date: new Date().toISOString()
-          });
-          fs.writeFileSync('usernameChanges.json', JSON.stringify(usernameChanges, null, 2));
 
-          // Embed'i güncelle
-          const updatedEmbed = new EmbedBuilder()
-            .setTitle('Üye başarıyla kayıt edildi.')
-            .addFields({name: `**Yetkili**`, value: `<@${message.author.id}>`},
-            {name: `**Kayıt Edilen Kullanıcı**`, value: `<@${user.id}>`},
-            {name: `**İsmi, yaşı**`, value: `${name} | ${age}`,
-          name: `Cinsiyeti`, value: `${interaction.customId === 'register_kiz' ? 'Kız' : 'Erkek'}`}
-            )
-            .setThumbnail(client.user.displayAvatarURL({ dynamic: true, format: "png", size: 1024 }))
-            .setTimestamp()
-          await interaction.update({ embeds: [updatedEmbed], components: [] });
-        }
+          collector.stop();
+        });
 
-        collector.stop();
+        collector.on('end', collected => {
+          if (collected.size === 0) {
+            sentMessage.edit({ content: 'Kayıt süresi doldu.', components: [] });
+          } else {
+            sentMessage.edit({ components: [] });
+          }
+        });
       });
-
-      collector.on('end', collected => {
-        if (collected.size === 0) {
-          sentMessage.edit({ content: 'Kayıt süresi doldu.', components: [] });
-        } else {
-          sentMessage.edit({ components: [] });
-        }
-      });
-    });
-}}
+  }
+}
