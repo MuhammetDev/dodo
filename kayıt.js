b281da3b863696ed6629bfabee9f38dd7590d0db
Muhammet Emin <66025604+MuhammetDev@users.noreply.github.com>
2024-07-20 22:48:32 +0300

    Update kayıt.js

--- kayıt.js
+++ kayıt.js
@@ -7,18 +7,16 @@ module.exports = {
   async execute(client, message, args) {
 
     // Gerekli rol ID'leri ve log kanalı ID'si
-    const kayitsizRoleID = '1264249388516249631'; // Kayıtsız rolünün ID'sini burada değiştirin
-    const kizRoleID = '1263218335361929256'; // Kız rolünün ID'sini burada değiştirin
-    const erkekRoleID = '1264249044071612500'; // Erkek rolünün ID'sini burada değiştirin
-    const logChannelID = '1242942289211560039'; // Log kanalının ID'sini burada değiştirin
-    const yetkiliRoleID = '1264252101937070242'; // Yetkili rolünün ID'sini burada değiştirin
+    const kayitsizRoleID = '1264249388516249631';
+    const kizRoleID = '1263218335361929256';
+    const erkekRoleID = '1264249044071612500';
+    const logChannelID = '1242942289211560039';
+    const yetkiliRoleID = '1264252101937070242';
 
-    // Komutu sadece belirli bir rolü olan yetkililer kullanabilir
     if (!message.member.roles.cache.has(yetkiliRoleID)) {
       return message.channel.send("Bu komutu kullanmak için yeterli izniniz yok.");
     }
 
-    // Komut argümanları kontrolü
     if (args.length < 2) {
       return message.channel.send("Lütfen şu formatta komutu kullanın: .kayıt <id/@üye> <isim>");
     }
@@ -26,7 +24,6 @@ module.exports = {
     const userMention = args[0];
     const name = args[1];
 
-    // Kullanıcıyı bulma
     let user;
     if (message.mentions.members.size) {
       user = message.mentions.members.first();
@@ -55,10 +52,13 @@ module.exports = {
     const embed = new EmbedBuilder()
       .setTitle(`Yeni bir üye kayıt ediliyor<a:exdoki_aaaaa:1262017677916901518>`)
       .setDescription(`**Lütfen cinsiyetinizi seçiniz`)
-      .addFields({name: `**Kayıt edilen kullanıcı**`, value: `<@${user.id}>`},
-        {name: `**İsmi**`, value: `\`${name}\``})
+      .addFields(
+        { name: `**Kayıt edilen kullanıcı**`, value: `<@${user.id}>` },
+        { name: `**İsmi**`, value: `\`${name}\`` }
+      )
       .setThumbnail(client.user.displayAvatarURL({ dynamic: true, format: "png", size: 1024 }))
-      .setTimestamp()
+      .setTimestamp();
+
     message.channel.send({ embeds: [embed], components: [row] })
       .then(sentMessage => {
         const collector = sentMessage.createMessageComponentCollector({ filter: i => i.user.id === message.author.id, time: 60000 });
@@ -95,17 +95,16 @@ module.exports = {
               logChannel.send({ embeds: [logEmbed] });
             }
 
-            
-            // Embed'i güncelle
             const updatedEmbed = new EmbedBuilder()
               .setTitle('Üye başarıyla kayıt edildi.')
-              .addFields({name: `**Yetkili**`, value: `<@${message.author.id}>`},
-              {name: `**Kayıt Edilen Kullanıcı**`, value: `<@${user.id}>`},
-              {name: `**İsmi**`, value: `${name}`},
-              {name: `Cinsiyeti`, value: `${interaction.customId === 'register_kiz' ? 'Kız' : 'Erkek'}`}
+              .addFields(
+                { name: `**Yetkili**`, value: `<@${message.author.id}>` },
+                { name: `**Kayıt Edilen Kullanıcı**`, value: `<@${user.id}>` },
+                { name: `**İsmi**`, value: `${name}` },
+                { name: `Cinsiyeti`, value: `${interaction.customId === 'register_kiz' ? 'Kız' : 'Erkek'}` }
               )
               .setThumbnail(client.user.displayAvatarURL({ dynamic: true, format: "png", size: 1024 }))
-              .setTimestamp()
+              .setTimestamp();
             await interaction.update({ embeds: [updatedEmbed], components: [] });
           }
 
